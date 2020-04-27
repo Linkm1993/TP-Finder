f5b892d63de3ea87d167f25deabc24d537c17787
Matt Link <Linkm1993@gmail.com>
2020-04-27 19:33:10 -0400

    Removed login js files

--- schema.sql
+++ schema.sql
@@ -1,3 +1,19 @@
--- Schema
-DROP DATABASE IF EXISTS passport_demo;
-CREATE DATABASE passport_demo;
+DROP DATABASE IF EXISTS tp_db;
+CREATE DATABASE tp_db;
+
+USE DATABASE tp_db;
+
+CREATE TABLE stock (
+    id INT AUTO_INCREMENT NOT NULL,
+    store_name VARCHAR(30),
+    availbility INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE posts(
+    id INT AUTO_INCREMENT NOT NULL,
+    sotre_id VARCHAR(30),
+    author VARCHAR(30),
+    msg VARCHAR(55),
+    PRIMARY KEY(id)
+);
