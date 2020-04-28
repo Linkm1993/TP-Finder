f2b5856e09fa3df5ce331ed4d88dbb2cd9a183d7
Matt Link <Linkm1993@gmail.com>
2020-04-27 21:26:24 -0400

    Adding back old template

--- schema.sql
+++ schema.sql
@@ -1,19 +1,3 @@
-DROP DATABASE IF EXISTS tp_db;
-CREATE DATABASE tp_db;
-
-USE tp_db;
-
-CREATE TABLE stores (
-    id INT AUTO_INCREMENT NOT NULL,
-    store_name VARCHAR(30),
-    availbility INT,
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE posts (
-    id INT AUTO_INCREMENT NOT NULL,
-    store_id VARCHAR(30),
-    author VARCHAR(30),
-    msg VARCHAR(55),
-    PRIMARY KEY(id)
-);
+-- Schema
+DROP DATABASE IF EXISTS passport_demo;
+CREATE DATABASE passport_demo;
