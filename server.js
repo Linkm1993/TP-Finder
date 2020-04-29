70394755085239fbcedd7094cbf6dd218b6b0522
kneves <55398273+kneves1@users.noreply.github.com>
2020-04-29 18:49:46 -0400

    installed dotenv, updated all relevent files to accommodate .env

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var session = require("express-session");
 var exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
+require('dotenv').config();
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
