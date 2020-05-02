5d3c80f4ee3df89810a3afd6484a05b9a4353e63
Matt Link <Linkm1993@gmail.com>
2020-05-02 10:45:42 -0400

    Updated post and store js files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var session = require("express-session");
 var exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+// var passport = require("./config/passport");
 require('dotenv').config();
 
 // Setting up port and requiring models for syncing
@@ -22,12 +22,12 @@ app.set("view engine", "handlebars");
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
