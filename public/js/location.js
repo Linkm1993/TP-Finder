a6627bb5f5276db185257199854680558191b89f
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-05-04 18:43:18 -0400

    adding location.js update

--- public/js/location.js
+++ public/js/location.js
@@ -1,4 +1,9 @@
+var map, datasource, popup, fullTP, results = [];
+
+
 function GetMap(){
+//cleanline = shopData.toString();
+//console.log(cleanline);
     //Instantiate a map object
     var map = new atlas.Map("myMap", {
         showFeedbackLink: false,
@@ -12,11 +17,26 @@ function GetMap(){
     });
     //Wait until the map resources are ready.
     map.events.add('ready', function() {
-         
+        //=====TESTING ADDRESS RETRIEVAL ===== ///
+       storeTest = [];
+       $.get("/api/stores", function(storeDataLogs) {
+        storesinfo = storeDataLogs;
+        console.log(storeDataLogs);
+        storesinfo.forEach(obj => {
+            uniqueTag = obj.id;
+            existingStore = obj.longlat;
+            console.log(existingStore);
+            userLocation.add(new atlas.data.Point([existingStore]));
+        })})
+//=====TESTING ADDRESS RETRIEVAL ===== ///
+
+       startSearch();
     //Load the custom image icon into the map resources.
     map.imageSprite.add('going-potty', 'https://cdn.shopify.com/s/files/1/0251/2525/7269/files/tp-hunter-lg.png').then(function () {
        userLocation = new atlas.source.DataSource();
        userLocation.add(new atlas.data.Point([lon, lat]));
+
+
        map.sources.add(userLocation);
         //var userSpot = new atlas.layer.SymbolLayer(userLocation, null, {
        map.layers.add(new atlas.layer.SymbolLayer(userLocation, null, {
@@ -28,32 +48,32 @@ function GetMap(){
            }
        }));
    });
-   console.log("the onestore command: " + oneStore);
+   //console.log("the onestore command: " + oneStore);
    //Load the custom image icon into the map resources.
+   
+   
 
-   fullTP = new atlas.source.DataSource();
-   map.sources.add(fullTP);
-   map.imageSprite.add('tp-full', 'https://cdn.shopify.com/s/files/1/0251/2525/7269/files/tp-full.png').then(function () {
-    trVal = trialValue.replace(/['"]+/g, ''); 
-    console.log(trVal);
-   fullTP.add(trialValue);
-      //fullTP.add(new atlas.data.Point([-75.82479, 39.59081]));
-      trialRUN;
-      console.log(trialValue);
-      
-       map.layers.add(new atlas.layer.SymbolLayer(fullTP,{
+   //Add a layer for rendering data.
+   //map.layers.add(new atlas.layer.SymbolLayer(existingLocations));
+   existingLocations = new atlas.source.DataSource();
+   map.sources.add(existingLocations);
+    map.imageSprite.add('tp-full', 'https://cdn.shopify.com/s/files/1/0251/2525/7269/files/tp-full.png');
+    
+    var fullTP = new atlas.layer.SymbolLayer(existingLocations,null, {
            iconOptions: {
                image: 'tp-full',
                anchor: 'center',
                size: 0.4,
                allowOverlap: false
            }
-        }));
         
-        });
+         });
+         
+         map.layers.add(fullTP);
     //Create a data source and add it to the map.
     datasource = new atlas.source.DataSource();
     map.sources.add(datasource);
+  
 
          
     //Add a layer for rendering point data.
@@ -72,7 +92,6 @@ function GetMap(){
 
     //map.layers.add(userSpot);
     map.layers.add(resultLayer);
-    
     popup = new atlas.Popup();
     
     //Add a mouse over event to the result layer and display a popup when this event fires.
@@ -80,7 +99,7 @@ function GetMap(){
     });
     
     var pipeline = atlas.service.MapsURL.newPipeline(new atlas.service.SubscriptionKeyCredential(atlas.getSubscriptionKey()));
-    
+    searchLat = new atlas.service.SearchURL(pipeline);
     searchURL = new atlas.service.SearchURL(pipeline);
     // Latitude & Longitude are provided by the 'map.js' script for geolocation function
     var query =  'grocery-store';
@@ -133,7 +152,30 @@ function GetMap(){
             <div><b>${p.poi.name}</b></div>
             <div>${p.address.freeformAddress}</div>
             <div>${position[1]}, ${position[0]}</div>
-          </div>`
+          </div>
+          <form name="radio-buttons">
+          <div class="row">
+            <label class="col">
+              <input type="radio" name="tp-status" value="0">
+              <img class="col" src="img/empty-roll.PNG" alt="no toilet paper">
+            </label>
+            
+            <label class="col">
+              <input type="radio" name="tp-status" value="1">
+              <img class="col" src="img/toilet-paper.svg" alt="moderate amount of toilet paper">
+            </label>
+
+            <label class="col">
+              <input type="radio" name="tp-status" value="2">
+              <img class="col" src="img/tower-of-tp.PNG" alt="tons of toilet paper">
+            </label>
+
+            <!-- Left as default selected radio button incase user doesn't select the options above. So we can identify junk in our db -->
+            <label>
+              <input type="radio" name="tp-status" value="3" checked>
+            </label>
+          </div>
+        </form>`
           ;
     
         //Update the content and position of the popup.
@@ -150,7 +192,7 @@ function GetMap(){
             store_name: p.poi.name,
               uniqueID: store_ID,
               availability: store_Inventory,
-              longlat : "["+store_Long +", "+store_Lat+"]"
+              longlat : store_Long +","+ store_Lat
             };
             $.ajax({
                 method: "POST",
@@ -165,4 +207,92 @@ function GetMap(){
         //Open the popup.
         popup.open(map);
     }
+
+
+//============================== ADDED BY RICARDO TO GET LOGLAT
+
+var map, existingLocations, searchLat;
+var start, end, isBusy = false;
+       
+var results = [];
+
+var searchOptions = {
+    view: 'Auto',
+    limit: 1    //Only need one result per address.
+};
+
+//Set of addresses to geocode.
+//var latlong = [cleanline];
+//console.log(latlong.length);
+
+
+function startSearch() {
+    if (!isBusy) {
+        isBusy = true;
+        results = [];
+
+        start = window.performance.now();
+
+                parallelGeocode();
+
+        }
+    }
+
+/**
+ * This method will iterate through all the locations and make multiple parallel requests.
+ * The browser will limit the number of concurrent requests to the same domain.
+ */
+async function parallelGeocode() {
+    var requests = [];
+    var searchOptions = {
+        view: 'Auto',
+        limit: 1    //Only need one result per address.
+    };
+    //Create the request promises.
+
+        $.get("/api/stores", function(storeDataLogs) {
+            
+            storesinfo = storeDataLogs;
+            console.log(storeDataLogs);
+            storesinfo.forEach(obj => {
+                uniqueTag = obj.id;
+                existingStore = obj.longlat;
+                userLocation.add(new atlas.data.Point([existingStore]));
+                
+                //shopData.push(existingStore);
+                requests.push(searchLat.searchAddressReverse(atlas.service.Aborter.timeout(10000), existingStore, searchOptions));
+                console.log(requests);
+            })})
+
+
+
+
+
+
+    // for (var i = 0; i < latlong.length; i++) {
+    //     requests.push(searchLat.searchAddressReverse(atlas.service.Aborter.timeout(10000), latlong[i]));
+    // }
+
+    //Process the promises in parallel.
+    var responses = await Promise.all(requests);
+
+    //Extract the GeoJSON feature results.
+    responses.forEach(r => {
+        var fc = r.geojson.getFeatures();
+
+        if (fc.features.length > 0) {
+            results.push(fc.features[0]);
+        }
+    });
+    existingLocations.add(results);
+    console.log(results);
+    isBusy = false;
+    //Done.
+   endSearch();
+}
+
+
+// function endSearch() {
+
+// }
 }
