8740634bf73a326d83775933ccdaa10e093e4194
Kendra Neves <kendraneves@gmail.com>
2020-05-05 10:56:28 -0400

    Getting store ID from api

--- public/js/map.js
+++ public/js/map.js
@@ -19,7 +19,7 @@ GetMap();
 
 $("#submit-button").click(function() {
   var radioValue = $("input[name='tp-status']:checked").val();
-  var storeID = $("#exampleModalLongTitle").data("TODO"); //don't know  p.poi
+  var storeID = $("#exampleModalLongTitle").data("storeID");
   console.log("you chose"+ " " + radioValue);
   console.log(storeID);
   sendTPStatus(storeID, radioValue);
@@ -28,3 +28,7 @@ $("#submit-button").click(function() {
 function sendTPStatus(storeID, radioValue) {
   // var storeID = p.poi.id; //DONT ACTUALLY KNOW WHAT API VAL IS CALLED, JUST MADE UP
 }
+
+$( document ).ready(function() {
+  $('#search-input').trigger("click");
+});
