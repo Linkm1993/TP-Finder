1a1fa82f8c0064b97cd174fa03e31af78293bc26
Cosplay Diver <kaydeekhing@gmail.com>
2020-05-16 10:48:51 -0400

    Changed API key

--- public/js/location.js
+++ public/js/location.js
@@ -50,7 +50,7 @@ function GetMap(){
        //Add your Azure Maps subscription key to the map SDK. Get an Azure Maps key at https://azure.com/maps
        authOptions: {
            authType: 'subscriptionKey',
-           subscriptionKey: 'oZUx6Omt9NUikroHFTz8Ad3L9oNOBdAAsa19qIlPgNM'
+           subscriptionKey: 'FlsHUGstio7gLsUqtNe_qkgkVO_Lu5OzoCw829_N1dc'
        }
    });
    
